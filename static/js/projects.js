93b5ddcfe766460805ccfb53f7d7819e2d76518e
Anthony Johnson <aj@ohess.org>
2012-01-29 20:39:01 -0800

    Fixing project stats

--- static/js/projects.js
+++ static/js/projects.js
@@ -13,17 +13,19 @@ function get_project_stats(project) {
     req.open('POST', '/projects/'+ project +'/stats.json', true);
     req.onreadystatechange = function() {
         if (req.readyState == 4) {
+            var a = req.responseText;
             update_project_stats(
-                eval('('+req.responseXML+')')
+                eval('('+ req.responseText +')')
             );
         }
     }
     req.send();
 }
 
-function update_project_stats(stats){
-    document.getElementById('project-stats').innerHTML = sprintf(
-        'Build tests: '+ stats.failed +' failed, '+ stats.passed +' passed'
-    );
+function update_project_stats(stats) {
+    var tests_pass = stats.passed;
+    var tests_full = stats.passed + stats.failed;
+    document.getElementById('project-stats').innerHTML = 'Latest build: '+ 
+      tests_pass +'/'+ tests_full +' tests passed';
 }
 
